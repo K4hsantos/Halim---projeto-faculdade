c1c6119218472f79a6e9244b300918361aacf17a
K4hsantos <kasantoshenrique@gmail.com>
2025-06-01 15:23:48 -0300

    ajustes para teste final

--- img.js
+++ img.js
@@ -1,4 +1,4 @@
-const maps = document.querySelector(".maps")
+const maps = document.querySelector(".img")
 maps.addEventListener("click", () => {
 window.location.href = "https://www.google.com/maps/place/R.+Dr.+Rafael+de+Barros,+56+-+Para%C3%ADso,+S%C3%A3o+Paulo+-+SP,+04003-040,+Brasil/@-23.571915,-46.644969,16z/data=!4m6!3m5!1s0x94ce59963ee0bfb9:0xc46ae92986de7e75!8m2!3d-23.5719149!4d-46.6449691!16s%2Fg%2F11rg68dqm7?hl=pt-BR&entry=ttu&g_ep=EgoyMDI1MDUyOC4wIKXMDSoASAFQAw%3D%3D";
 })
